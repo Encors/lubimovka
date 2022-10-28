82dffd8f3e20c22ae182ca84dcdcf3b541683eba
Egor Popikov <cool.egor-kot@yandex.ru>
2022-10-28 14:50:55 +0300

    feat: добавить секцию plays и горизонт.скролл

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+//горизонтальный скролл секции plays
+
+function scrollHorizontally(e) {
+    e = window.event || e;
+    let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    document.querySelector('.plays__container').scrollLeft -= (delta * 35);
+    e.preventDefault();
+}
+if (document.querySelector('.plays__container').addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    document.querySelector('.plays__container').addEventListener("mousewheel", scrollHorizontally, false);
+    // Firefox
+    document.querySelector('.plays__container').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+} else {
+    // IE 6/7/8
+    document.querySelector('.plays__container').attachEvent("onmousewheel", scrollHorizontally);
+}
+
+scrollHorizontally()
