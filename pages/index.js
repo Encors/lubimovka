6d84bc8248f6f111d0240ae305031d7cdd5fdbdb
Denis Nikolskiy <den-apple@ya.ru>
2022-11-05 11:53:37 +0200

    refactor index.js

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,3 @@
-//горизонтальный скролл секции plays
-const scrollBlock = document.querySelector('.plays__container');
-
-function scrollHorizontally(e) {
-  e = window.event || e;
-  let delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
-  document.querySelector('.plays__container').scrollLeft -= delta * 35;
-  if (this.scrollWeight - this.scrollLeft === this.clientWeight) {
-    e.preventDefault();
-  } else if (this.scrollWeight - this.scrollRight === this.clientWeight) {
-    e.preventDefault();
-  }
-}
-if (document.querySelector('.plays__container').addEventListener) {
-  // IE9, Chrome, Safari, Opera
-  document
-    .querySelector('.plays__container')
-    .addEventListener('mousewheel', scrollHorizontally, false);
-  // Firefox
-  document
-    .querySelector('.plays__container')
-    .addEventListener('DOMMouseScroll', scrollHorizontally, false);
-} else {
-  // IE 6/7/8
-  document
-    .querySelector('.plays__container')
-    .attachEvent('onmousewheel', scrollHorizontally);
-}
-
-scrollHorizontally();
-
 import CardSpectacle from '../components/CardSpectacle.js';
 import Section from '../components/Section.js';
 
@@ -129,3 +98,34 @@ function renderSpectaclesList() {
 }
 
 mediaQuery.addEventListener('change', changeStyleToMobile);
+
+//горизонтальный скролл секции plays
+const scrollBlock = document.querySelector('.plays__container');
+
+function scrollHorizontally(e) {
+  e = window.event || e;
+  let delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+  document.querySelector('.plays__container').scrollLeft -= delta * 35;
+  if (this.scrollWeight - this.scrollLeft === this.clientWeight) {
+    e.preventDefault();
+  } else if (this.scrollWeight - this.scrollRight === this.clientWeight) {
+    e.preventDefault();
+  }
+}
+if (document.querySelector('.plays__container').addEventListener) {
+  // IE9, Chrome, Safari, Opera
+  document
+    .querySelector('.plays__container')
+    .addEventListener('mousewheel', scrollHorizontally, false);
+  // Firefox
+  document
+    .querySelector('.plays__container')
+    .addEventListener('DOMMouseScroll', scrollHorizontally, false);
+} else {
+  // IE 6/7/8
+  document
+    .querySelector('.plays__container')
+    .attachEvent('onmousewheel', scrollHorizontally);
+}
+
+scrollHorizontally();
